ef4bc53b46db5ed85caf2c99fe007a4e01a7ae46
pabluh <pablohmendes@gmail.com>
2022-02-22 22:27:31 -0300

    chore: removing test component + adding Dashboard

--- App.tsx
+++ App.tsx
@@ -1,21 +1,6 @@
-import React from 'react';
-import { StyleSheet, View, StatusBar } from 'react-native';
-import { Welcome } from './src/Components/Welcome';
+import React from "react";
+import { Dashboard } from "./src/Screens/Dashboard";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar backgroundColor="#fff" />
-      <Welcome title="React Native Bare Workflow com Typescript" />
-    </View>
-  );
+  return <Dashboard />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
