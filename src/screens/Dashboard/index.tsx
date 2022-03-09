ab4e6647d47a5838989b7a1d19ffb4ac6c0c5709
pabluh <pablohmendes@gmail.com>
2022-03-08 22:59:21 -0300

    chore: show info from user in header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,34 @@
-import React from "react";
-import { Container, Title } from "./styles";
+import React from 'react'
+import { Text } from 'react-native'
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/260357?v=4'
+              }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Usuário</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
-  );
+  )
 }
